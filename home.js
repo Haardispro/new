beafbc47808a09ae023a9143eed467b48f0ced53
Haardispro <62053026+Haardispro@users.noreply.github.com>
2021-05-23 15:38:33 +0530

    hello

---
+++ home.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+function hide() {
+    var a = document.getElementById('dis');
+    var b = document.getElementById('name');
+    if (b.innerHTML = 'I am 14 years old'){
+      b.innerHTML = 'Haard Majmudar';
+    }
+    if (a.innerHTML = 'I live in India. And I am learning Python, C/C++, HTML/CSS and Javascript') {
+      a.innerHTML = 'Student and Aspiring Programmer';
+    }
+    
+}
+  function about() {
+    
+    var a = document.getElementById('name');
+    var b = document.getElementById('dis');
+    a.innerHTML = 'I am 14 years old';
+    b.innerHTML = 'I live in India. And I am learning Python, C/C++, HTML/CSS and Javascript';
+    
+    
+}
